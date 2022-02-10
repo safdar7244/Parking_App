536dc726d91d93b1cc4cc1c768111bb428fdb5fa
amnaarshed <amna.arshed4224@gmail.com>
2022-02-10 23:38:17 +0500

    updates generate invoice link

--- screens/Parked.js
+++ screens/Parked.js
@@ -175,7 +175,7 @@ function Parked(props, navigation) {
     } else {
       try {
         const data = await axios.post(
-          "https://salty-citadel-46447.herokuapp.com/generate_invoice",
+          "https://ancient-woodland-88729.herokuapp.com/generate_invoice",
           {
             name: doc.data().name,
             email: doc.data().email,
